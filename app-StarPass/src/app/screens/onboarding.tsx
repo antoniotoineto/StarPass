5aa720186c9859c8e37ac4d70dfef9138582c438
antoniotoineto <netocaastro10@gmail.com>
2024-11-18 15:54:12 -0300

    #1 Starting steps of how to use screen

--- app-StarPass/src/app/screens/onboarding.tsx
+++ app-StarPass/src/app/screens/onboarding.tsx
@@ -1,13 +1,69 @@
 import { Link } from 'expo-router';
 import React, { useState} from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Animated } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { useRouter } from 'expo-router';
+
 
 export default function OnboardingScreen() {
+  const router = useRouter();
+  const [currentStep, setCurrentStep] = useState(0);
+  const [visibleSteps, setVisibleSteps] = useState([false, false, false]);
+  const fadeAnim = [new Animated.Value(0), new Animated.Value(0), new Animated.Value(0)];
+
+  const steps: { id: number; content: string; position: 'top' | 'middle' | 'bottom' }[]  = [
+    { id: 1, content: 'Insira sua senha de entrada para obter acesso completo ao aplicativo.', position: 'top' },
+    { id: 2, content: 'Procure o brinquedo desejado e entre na fila.', position: 'middle' },
+    { id: 3, content: 'Acompanhe o status da sua fila e, ao chegar sua vez, apresente o QR Code na entrada.', position: 'bottom' },
+  ];
+
+  const handleNextStep = () => {
+    if (currentStep < steps.length - 1) {
+      const updatedSteps = [...visibleSteps];
+      updatedSteps[currentStep] = true;
+      setVisibleSteps(updatedSteps);
+
+      Animated.timing(fadeAnim[currentStep], {
+        toValue: 1,
+        duration: 500,
+        useNativeDriver: true,
+      }).start();
+
+      setCurrentStep(currentStep + 1);
+    } else {
+      router.push('/screens/attractionsList');
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Aooo</Text>
-      <Link href='/screens/getEntryCode'>Simbora</Link>
+        <Text style={styles.text}>Logo</Text>
+        <View style={styles.infoContainer}>
+            {steps.map((step, index) => (
+                <Animated.View
+                    key={step.id}
+                    style={[
+                        styles.stepContainer,
+                        styles[step.position],
+                        { opacity: fadeAnim[index] },
+                    ]}
+                >
+                    {visibleSteps[index] && <Text style={styles.stepText}>{step.content}</Text>}
+                </Animated.View>
+            ))}
+        </View>
+        <View style={styles.buttonsContainer}>
+            <Link  href={"/screens/getEntryCode"} asChild>
+                <TouchableOpacity style={styles.skipButton}>
+                    <Text style={{fontSize: 20}}>Pular</Text>
+                </TouchableOpacity>
+            </Link>
+            <TouchableOpacity style={styles.nextButton} onPress={handleNextStep}>
+                <Text style={{fontSize: 25}}>{currentStep === steps.length ? 'Iniciar' : 'Próximo'}</Text>
+            </TouchableOpacity>
+        </View>
+        
+        <StatusBar style="auto" />
     </View>
   );
 }
@@ -17,7 +73,65 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'space-around',
     alignItems: 'center',
-    padding: 50,
     backgroundColor: '#fff',
+    padding: 35,
+  },
+  text: {
+    fontSize: 60,
+  },
+  infoContainer: {
+    width: '100%',
+    height: 500,
+    alignItems: 'center',
+    padding: 15,
+    borderWidth: 1,
+    borderColor: 'grey',
+    borderRadius: 15
+  },
+  stepContainer: {
+    position: 'absolute',
+    width: '100%',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  top: {
+    top: '10%',
+  },
+  middle: {
+    top: '40%',
+  },
+  bottom: {
+    top: '70%',
+  },
+  stepText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',
+    textAlign: 'center',
+    backgroundColor: '#ffffff',
+    padding: 10,
+    borderRadius: 8,
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+  buttonsContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  skipButton: {
+    backgroundColor: '#e3e3e3',
+    padding: 10,
+    justifyContent: 'center',
+    borderRadius: 10
+  },
+  nextButton: {
+    backgroundColor: '#b0b0b0',
+    padding: 10,
+    borderRadius: 10
   }
+
 });
