14c0e42409476971650b5c8c61bf7754f4afdae8
antoniotoineto <netocaastro10@gmail.com>
2024-12-12 14:58:02 -0300

    #10 Adding styles to Select Gate e Gates Code screens

--- services-StarPass/admin-panel/src/screens/GateCodes.tsx
+++ services-StarPass/admin-panel/src/screens/GateCodes.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import { IoMdArrowBack } from "react-icons/io";
+
 
 const GateCodes: React.FC = () => {
     const { gate } = useParams<{ gate: string }>();
@@ -18,23 +20,117 @@ const GateCodes: React.FC = () => {
         };
 
         fetchCodes();
+
+        const intervalId = setInterval(fetchCodes, 5000);
+
+        return () => clearInterval(intervalId);
     }, [gate]);
 
     return (
-        <div style={{ textAlign: "center", marginTop: "50px" }}>
-            <h1>Códigos do Portão {gate}</h1>
-            {error && <p style={{ color: "red" }}>{error}</p>}
-            {codes.length > 0 ? (
-                <ul>
-                    {codes.map((code, index) => (
-                        <li key={index}>{code}</li>
-                    ))}
-                </ul>
-            ) : (
-                <p>Nenhum código encontrado para este portão.</p>
-            )}
+        <div style={styles.container}>
+            <div style={styles.card}>
+                <div style={styles.topBar}>
+                    <IoMdArrowBack size={50} style={styles.icon} />
+                    <h1 style={styles.logo}>Logo</h1>
+                </div>
+                <h1>Guichê {gate}</h1>
+                {error && <p style={{ color: "red" }}>{error}</p>}
+                {codes.length > 0 ? (
+                    <div style={styles.scrollContainer}>
+                        <ul style={styles.codeList}>
+                            {codes.map((code, index) => (
+                                <li key={index} style={index === 0 ? styles.firstCodeItem : styles.codeItem}>
+                                    {code}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                ) : (
+                    <p style={styles.emptyMessage}>Nenhum código encontrado para este portão.</p>
+                )}
+            </div>
         </div>
     );
+};
+
+const styles = {
+    container: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "85vh",
+        backgroundColor: "#e6e6e6",
+        padding: "40px",
+
+    },
+    topBar: {
+        display: "flex",
+        flexDirection: "row" as "row", 
+        width: "100%",
+        height: 50,
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: "0 20px", 
+    },
+    icon: {
+        cursor: "pointer", 
+    },
+    card: {
+        display: "flex",
+        flexDirection: "column" as "column",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "20px",
+        padding: "20px",
+        backgroundColor: "white",
+        borderRadius: 18
+    },
+    logo: {
+        fontSize: "2rem",
+        fontWeight: "bold" as "bold",
+    },
+    scrollContainer: {
+        width: "100%",
+        maxHeight: "200px",
+        overflowY: "auto" as "auto",
+        padding: "10px",
+        backgroundColor: "white",
+        display: "flex",
+        flexDirection: "column" as "column",
+        gap: "10px",
+    },
+    codeList: {
+        listStyleType: "none",
+        padding: 0,
+        margin: 0,
+        display: "flex",
+        flexDirection: "column" as "column",
+        gap: "10px",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    firstCodeItem: {
+        backgroundColor: "#cccccc",
+        borderRadius: "10px",
+        padding: "10px",
+        fontSize: "2rem",
+        fontWeight: "bold" as "bold",
+        textAlign: "center" as "center",
+        minWidth: "50px",
+        width: 300,
+    },
+    codeItem: {
+        backgroundColor: "#e3e3e3",
+        borderRadius: "10px",
+        padding: "10px",
+        fontSize: "1.5rem",
+        textAlign: "center" as "center",
+        minWidth: "50px",
+        width: 250
+    },
+    emptyMessage: {
+        color: 'red'
+    }
 };
 
 export default GateCodes;
--- services-StarPass/admin-panel/src/screens/SelectGate.tsx
+++ services-StarPass/admin-panel/src/screens/SelectGate.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { AiOutlineSelect } from 'react-icons/ai';
 
 const SelectGate: React.FC = () => {
     const [selectedGate, setSelectedGate] = useState<string>("0");
@@ -7,7 +8,7 @@ const SelectGate: React.FC = () => {
     const navigate = useNavigate();
 
     const handleSelection = () => {
-        if(selectedGate==="0"){
+        if (selectedGate === "0") {
             setError(true)
         } else {
             navigate(`/codes/${selectedGate}`);
@@ -15,21 +16,83 @@ const SelectGate: React.FC = () => {
     };
 
     return (
-        <div style={{ textAlign: "center", marginTop: "50px" }}>
-            <h1>Selecione o Guichê</h1>
-            <select value={selectedGate} onChange={(e) => setSelectedGate(e.target.value)}>
-                <option value="0">Selecione</option>
-                <option value="1">Guichê 1</option>
-                <option value="2">Guichê 2</option>
-                <option value="3">Guichê 3</option>
-            </select>
-            <br />
-            {error && <p style={{ color: "red" }}>Selecione um guichê para continuar</p>}
-            <button onClick={handleSelection} style={{ marginTop: "20px" }}>
-                Ver Códigos
-            </button>
+        <div style={styles.container}>
+            <div style={styles.card}>
+                <h1 style={styles.logo}>Logo</h1>
+                <h2 style={styles.subtitle}>
+                    Selecione o guichê para mostrar as senhas de entrada
+                </h2>
+                <AiOutlineSelect size={50} style={styles.icon} />
+                <select
+                    style={styles.select}
+                    value={selectedGate}
+                    onChange={(e) => setSelectedGate(e.target.value)}
+                >
+                    <option value="0">Selecione</option>
+                    <option value="1">Guichê 1</option>
+                    <option value="2">Guichê 2</option>
+                    <option value="3">Guichê 3</option>
+                </select>
+                {error && <p style={styles.error}>Selecione um guichê para continuar</p>}
+                <button style={styles.button} onClick={handleSelection}>
+                    Ver Códigos
+                </button>
+            </div>
         </div>
     );
+};
+
+const styles = {
+    container: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        backgroundColor: "#e6e6e6",
+        padding: "40px",
+
+    },
+    card: {
+        display: "flex",
+        flexDirection: "column" as "column",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "20px",
+        padding: "20px",
+        backgroundColor: "white",
+        borderRadius: 18
+    },
+    logo: {
+        fontSize: "2rem",
+        fontWeight: "bold" as "bold",
+    },
+    subtitle: {
+        fontSize: "1.2rem",
+        textAlign: "center" as "center",
+    },
+    icon: {
+        marginBottom: "10px",
+    },
+    select: {
+        width: "80%",
+        maxWidth: "400px",
+        padding: "10px",
+        fontSize: "1rem",
+    },
+    error: {
+        color: "red",
+        fontSize: "0.9rem",
+        marginTop: "-10px",
+    },
+    button: {
+        padding: "10px 20px",
+        fontSize: "1rem",
+        backgroundColor: "#007BFF",
+        color: "#FFF",
+        border: "none",
+        borderRadius: "5px",
+        cursor: "pointer",
+    },
 };
 
 export default SelectGate;
