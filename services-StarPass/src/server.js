e1ff98d3ae7b59641a9c08f64da3f93d05dfbc78
antoniotoineto <netocaastro10@gmail.com>
2025-02-04 19:03:12 -0300

    #37 Adding build in main server

--- services-StarPass/src/server.js
+++ services-StarPass/src/server.js
@@ -2,5 +2,5 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor em execução na porta ${PORT}`);
 });
