b5c28154ef78f32d2c2cfdd43fab76de58328f2f
antoniotoineto <netocaastro10@gmail.com>
2025-02-04 23:50:27 -0300

    Setting new BaseURL to app requests

--- app-StarPass/src/app/data/api.tsx
+++ app-StarPass/src/app/data/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.58:5000', 
+  baseURL: process.env.EXPO_PUBLIC_API_BASE_URL, 
   headers: {
     'Content-Type': 'application/json',
   },
