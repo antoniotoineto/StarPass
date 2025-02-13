67f840b473f58bacc849d5ae2acc6db7a6c6af5a
antoniotoineto <netocaastro10@gmail.com>
2025-02-12 23:01:08 -0300

    Fixing useCallBack in userQueues

--- app-StarPass/src/app/screens/queueList.tsx
+++ app-StarPass/src/app/screens/queueList.tsx
@@ -1,21 +1,56 @@
 import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
-import { useEffect, useState } from 'react';
+import { Alert, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useEffect } from 'react';
+import api from '../data/api';
 import TopBar from '../components/topBar';
 import QueueCard from '../components/queueCard';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useUserQueues } from '../hooks/fetchUserQueues';
+import { usePin } from '../context/pinCodeContext';
 
 export default function QueueListScreen() {
   const { userQueues, isEmpty, fetchQueues } = useUserQueues();
+  const { pin } = usePin();
 
   useEffect(() => {
     fetchQueues();
     const interval = setInterval(fetchQueues, 5000);
     return () => clearInterval(interval);
   }, []);
+
+  const handleDeleteQueue = (attractionId: string) => {
+    Alert.alert(
+      "Sair da fila",
+      "Tem certeza que deseja sair dessa fila?",
+      [
+        {
+          text: "Cancelar",
+          style: "cancel",
+        },
+        {
+          text: "Sair",
+          onPress: async () => {
+            try {
+              const res = await api.post(`/filas/sair-fila/${pin}/${attractionId}`);
+              console.log(res.data.message);
+              fetchQueues();
+
+            } catch (error: any) {
+              if (error.response) {
+                console.error('Erro no servidor:', error.response.data);
+                console.log("Erro: ", error.response.status)
+              } else {
+                console.error('Erro inesperado:', error.message);
+              }
+            }
+          },
+        },
+      ]
+    );
+
+  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -34,15 +69,20 @@ export default function QueueListScreen() {
             <Text style={styles.warningText}>Você não está em nenhuma fila no momento.</Text>
           ) : (
             userQueues.map((queue, key) => (
-              <QueueCard
-                key={key}
-                number={key + 1}
-                id={queue.id}
-                title={queue.title}
-                queuePosition={queue.queuePosition}
-                estimatedTime={queue.estimatedTime}
-                wave={queue.wave}
-              />
+              <View style={styles.cardContainer}>
+                <QueueCard
+                  key={key}
+                  number={key + 1}
+                  id={queue.id}
+                  title={queue.title}
+                  queuePosition={queue.queuePosition}
+                  estimatedTime={queue.estimatedTime}
+                  wave={queue.wave}
+                />
+                <TouchableOpacity onPress={() => handleDeleteQueue(queue.id)}>
+                  <Ionicons name="trash-outline" size={23} color="black" style={{ marginLeft: 5 }} />
+                </TouchableOpacity>
+              </View>
             ))
           )}
         </View>
@@ -74,6 +114,11 @@ const styles = StyleSheet.create({
     width: '100%',
     alignItems: 'flex-start',
     minHeight: 360
+  },
+  cardContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '80%',
   },
   titleContainer: {
     flexDirection: 'row',
